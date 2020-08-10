7672c3ce5a0dbd2060cadb5a1d5878da7ee872f2
Tejendre Aubeeluck <tejen.au@outlook.com>
2020-08-10 18:53:31 +0800

    landingpage

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -5,7 +5,14 @@ Page({
    * Page initial data
    */
   data: {
-
+    images: [
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/84c474e94ef95d65e638/teamBorderless.png",
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/79713925cb2da5e1fea9/fantasticfungi.png",
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/7d429cc031d872ed02a6/pastry.png",
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/3b8ad36243bd4ffc25dd/beer%20night.png",
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/b8efd796301c2ec1a6b6/madame%20tussaud.png",
+        "http://lc-z1O50gmk.cn-n1.lcfile.com/2be242541c685181179c/Screenshot%202020-08-10%20at%2017.29.52.png"
+    ]
   },
 
   /**
